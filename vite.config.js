5bd9bf9f42bfab93eca96edd80d9a11f04e97ad8
hoang-nh279 <uhoangnguyen2709@gmail.com>
2025-11-03 15:14:12 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:'/git-redux-demo/',
+  base:'/git-react-test-functions/',
   test: {
     globals: true,                 // Cho phép dùng describe, it, expect
     environment: 'jsdom',          // Quan trọng: có DOM cho React
