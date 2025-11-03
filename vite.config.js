5d7a57f43ebfbad4d524761ed3065ca1eb53b8e2
hoang-nh279 <uhoangnguyen2709@gmail.com>
2025-11-03 10:03:40 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,                 // Cho phép dùng describe, it, expect
+    environment: 'jsdom',          // Quan trọng: có DOM cho React
+    include: ['src/**/*.{test,spec}.{js,jsx}'], // Đảm bảo Vitest tìm thấy file test
+    setupFiles: './src/setupTests.js', // (Tùy chọn)
+  },
+})
